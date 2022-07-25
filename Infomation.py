af72c743a7a3ab5a624e83d5e32dfa4a196ba522
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-07-25 10:59:08 +0700

    Init

---
+++ Infomation.py
@@ -0,0 +1,7 @@
+Name=input("Enter Your Name:")
+birth_year=int(input("Birth year:"))
+Job=input("Your Current Job: ")
+age=2022-birth_year
+print("Name",Name)
+print("Age is:",age,"yearold")
+print("Job",Job)
